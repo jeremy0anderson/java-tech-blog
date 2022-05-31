dc9930696ae11a84049d8b91e77e95334d47c2d7
jeremy0anderson <projects@jeremyanderson.dev>
2022-05-31 03:11:20 -0600

    initial setup

---
+++ src/main/java/com/jdev/refactortechblog/RefactorTechBlogApplication.java
@@ -0,0 +1,13 @@
+package com.jdev.refactortechblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorTechBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorTechBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdev/refactortechblog/RefactorTechBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdev.refactortechblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorTechBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
